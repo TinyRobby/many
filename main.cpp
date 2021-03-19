97662d6da5a62822e347245d8a0b9a1056a8f8e3
The one and only one <spahatticat246@gmail.com>
2021-03-19 20:23:32 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Welcome to RH-cofusihedron!";
+  string a;
+  cout << a << endl;
+  cin >> a;
+  cout << a << endl;
+  cout << "huh! How could you!" << endl;
+  system("python henry.py && bash main.sh");
+}
---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo hello again
+echo whats your name?
+read confuse
+if confuse == "end"
+  echo NO!
+python henry.py  
+echo done
